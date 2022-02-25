67f37804716c710f1c953427e57342413881ca73
E-K8 <ekatplat@gmail.com>
2022-02-25 15:51:08 +0000

    add LoginButton, LogoutButton, Profile components install Auth, .env with auth details. Not perfect yet

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,18 +1,19 @@
-
 import Nav from '../Nav';
-
 import WorkoutDisplay from '../WorkoutDisplayCard';
+import LoginButton from '../LoginButton';
+import LogoutButton from '../LogoutButton';
+import Profile from '../Profile';
 
 import './App.scss';
 
 function App() {
   return (
     <div className="app">
-
       <Nav />
-
       <WorkoutDisplay />
-
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
     </div>
   );
 }
