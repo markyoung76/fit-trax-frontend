d8c03a90dddb763901fa46a0c2b4b79040a7f6ac
EzyhB <ezyhbmail@gmail.com>
2022-03-02 14:29:00 +0000

    added functionality for adding and removing text fields

--- src/components/CreateExercise/index.tsx
+++ src/components/CreateExercise/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Container, Grid, TextField } from '@mui/material';
+import { Button, Container, Grid, TextField, Typography } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import ClearIcon from '@mui/icons-material/Clear';
 import React from 'react';
@@ -9,11 +9,35 @@ import { Box } from '@mui/material';
 export default function CreateExercise() {
   const [exerciseNumber, setExerciseNumber] = useState([1]);
 
+  const handleAddExerciseClick = () => {
+    const newArray = [...exerciseNumber, 1];
+    console.log(newArray);
+
+    setExerciseNumber(newArray);
+  };
+
+  const handleRemoveExerciseClick = () => {
+    if (exerciseNumber.length > 1) {
+      const newArray = [...exerciseNumber];
+      newArray.pop();
+      setExerciseNumber(newArray);
+    }
+  };
+
+  const handleSubmit = () => {
+    const newExercises = [{}];
+  };
+
   return (
     <Container maxWidth="md">
       <Grid container>
         {exerciseNumber.map((number, index) => (
-          <Container id={number.toString()} key={index}>
+          <Box
+            id={number.toString()}
+            key={index}
+            className={css.gridMargin}
+            sx={{ width: '100%', display: 'flex', flexWrap: 'wrap' }}
+          >
             <TextField fullWidth required label="Exercise Name" />
             <Grid className={css.gridMargin} sx={{ display: 'flex' }} item xs={4}>
               <TextField className={css.smallInputFields} required label="Sets" sx={{ maxWidth: '7rem' }} />
@@ -24,16 +48,17 @@ export default function CreateExercise() {
             <Grid className={css.gridMargin} sx={{ display: 'flex', justifyContent: 'end' }} item xs={4}>
               <TextField className={css.smallInputFields} required label="Rest Period" />
             </Grid>
-          </Container>
+          </Box>
         ))}
-
+      </Grid>
+      <Grid container>
         <Grid item xs={6} sx={{ display: 'flex', justifyContent: 'center' }}>
-          <Button variant="outlined" sx={{ borderRadius: '1rem' }}>
+          <Button variant="outlined" sx={{ borderRadius: '1rem' }} onClick={handleRemoveExerciseClick}>
             <ClearIcon />
           </Button>
         </Grid>
         <Grid item xs={6} sx={{ display: 'flex', justifyContent: 'center' }}>
-          <Button variant="contained" sx={{ borderRadius: '1rem' }}>
+          <Button variant="contained" sx={{ borderRadius: '1rem' }} onClick={handleAddExerciseClick}>
             <AddIcon />
           </Button>
         </Grid>
