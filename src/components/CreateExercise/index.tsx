364a4e2cd34b88231c66b696294315bdd3717445
EzyhB <ezyhbmail@gmail.com>
2022-03-02 16:39:35 +0000

    added the form and got the 1st part of the functionality working

--- src/components/CreateExercise/index.tsx
+++ src/components/CreateExercise/index.tsx
@@ -5,6 +5,8 @@ import React from 'react';
 import css from './CreateExercise.module.css';
 import { useState } from 'react';
 import { Box } from '@mui/material';
+import DoneIcon from '@mui/icons-material/Done';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 
 export default function CreateExercise() {
   const [exerciseNumber, setExerciseNumber] = useState([1]);
@@ -23,76 +25,101 @@ export default function CreateExercise() {
       setExerciseNumber(newArray);
     }
   };
-
-  const handleSubmit = () => {
+  //
+  const handleSubmit = (e: React.FormEvent<HTMLInputElement>) => {
+    // e.preventDefault();
     const newExercises = [{}];
   };
 
   return (
     <Container maxWidth="md">
-      <Grid container>
-        {exerciseNumber.map((number, index) => (
-          <Box
-            id={number.toString()}
-            key={index}
-            className={css.gridMargin}
-            sx={{ width: '100%', display: 'flex', flexWrap: 'wrap' }}
-          >
-            <TextField
-              fullWidth
-              required
-              label="Exercise Name"
-              sx={{ border: '5px solid #6296EA', borderRadius: '1.3rem' }}
-            />
-            <Grid className={css.gridMargin} sx={{ display: 'flex', margin: '1rem 0' }} item xs={4}>
-              <TextField
-                className={css.smallInputFields}
-                required
-                label="Sets"
-                sx={{ maxWidth: '7rem', border: '5px solid #6296EA', borderRadius: '1.3rem' }}
-              />
-            </Grid>
-            <Grid
+      <form
+        className={css.formStyle}
+        onSubmit={(e: React.SyntheticEvent) => {
+          e.preventDefault();
+          const event = e.target as HTMLFormElement;
+          for (let i = 0; i < 8; i += 2) {
+            const ourEvent = event[i] as HTMLInputElement;
+            console.log(ourEvent.value);
+          }
+          // console.log(e.target);
+        }}
+      >
+        <Grid container>
+          {exerciseNumber.map((number, index) => (
+            <Box
+              id={number.toString()}
+              key={index}
               className={css.gridMargin}
-              sx={{ display: 'flex', justifyContent: 'center', margin: '1rem 0' }}
-              item
-              xs={4}
+              sx={{ width: '100%', display: 'flex', flexWrap: 'wrap' }}
             >
               <TextField
-                className={css.smallInputFields}
+                type="text"
+                fullWidth
                 required
-                label="Reps"
+                label="Exercise Name"
                 sx={{ border: '5px solid #6296EA', borderRadius: '1.3rem' }}
               />
-            </Grid>
-            <Grid
-              className={css.gridMargin}
-              sx={{ display: 'flex', justifyContent: 'end', margin: '1rem 0' }}
-              item
-              xs={4}
-            >
-              <TextField
-                className={css.smallInputFields}
-                required
-                label="Rest Period"
-                sx={{ border: '5px solid #6296EA', borderRadius: '1.3rem' }}
-              />
-            </Grid>
-          </Box>
-        ))}
-      </Grid>
-      <Grid container>
-        <Grid item xs={6} sx={{ display: 'flex', justifyContent: 'center' }}>
-          <Button variant="outlined" sx={{ borderRadius: '1rem' }} onClick={handleRemoveExerciseClick}>
-            <ClearIcon />
-          </Button>
+              <Grid className={css.gridMargin} sx={{ display: 'flex', margin: '1rem 0' }} item xs={4}>
+                <TextField
+                  className={css.smallInputFields}
+                  required
+                  label="Sets"
+                  sx={{ maxWidth: '7rem', border: '5px solid #6296EA', borderRadius: '1.3rem' }}
+                />
+              </Grid>
+              <Grid
+                className={css.gridMargin}
+                sx={{ display: 'flex', justifyContent: 'center', margin: '1rem 0' }}
+                item
+                xs={4}
+              >
+                <TextField
+                  className={css.smallInputFields}
+                  required
+                  label="Reps"
+                  sx={{ border: '5px solid #6296EA', borderRadius: '1.3rem' }}
+                />
+              </Grid>
+              <Grid
+                className={css.gridMargin}
+                sx={{ display: 'flex', justifyContent: 'end', margin: '1rem 0' }}
+                item
+                xs={4}
+              >
+                <TextField
+                  className={css.smallInputFields}
+                  required
+                  label="Rest Period"
+                  sx={{ border: '5px solid #6296EA', borderRadius: '1.3rem' }}
+                />
+              </Grid>
+            </Box>
+          ))}
         </Grid>
-        <Grid item xs={6} sx={{ display: 'flex', justifyContent: 'center' }}>
-          <Button variant="contained" sx={{ borderRadius: '1rem' }} onClick={handleAddExerciseClick}>
-            <AddIcon />
-          </Button>
+        <Grid container>
+          <Grid item xs={6} sx={{ display: 'flex', justifyContent: 'center' }} className={css.gridMargin}>
+            <Button variant="outlined" sx={{ borderRadius: '1rem' }} onClick={handleRemoveExerciseClick}>
+              <ClearIcon />
+            </Button>
+          </Grid>
+          <Grid item xs={6} sx={{ display: 'flex', justifyContent: 'center' }} className={css.gridMargin}>
+            <Button variant="contained" sx={{ borderRadius: '1rem' }} onClick={handleAddExerciseClick}>
+              <AddIcon />
+            </Button>
+          </Grid>
+          <Grid item xs={6} sx={{ display: 'flex', justifyContent: 'center' }} className={css.gridMargin}>
+            <Button variant="outlined" sx={{ borderRadius: '1rem' }} color="secondary">
+              <ArrowBackIosNewIcon />
+            </Button>
+          </Grid>
+          <Grid item xs={6} sx={{ display: 'flex', justifyContent: 'center' }} className={css.gridMargin}>
+            <Button type="submit" variant="contained" sx={{ borderRadius: '1rem' }} color="secondary">
+              <DoneIcon />
+            </Button>
+          </Grid>
         </Grid>
-      </Grid>
+      </form>
     </Container>
   );
 }
