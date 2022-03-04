1f6e5901cb77b98d561decfee6b932b6c9010890
E-K8 <ekatplat@gmail.com>
2022-03-04 13:58:09 +0000

    move logo, change font size

--- src/components/CreateExercise/index.tsx
+++ src/components/CreateExercise/index.tsx
@@ -8,7 +8,6 @@ import { Box } from '@mui/material';
 import DoneIcon from '@mui/icons-material/Done';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import { json } from 'stream/consumers';
-
 interface exercisesOBJ {
   exercise_name: string;
   sets: number;
@@ -19,10 +18,8 @@ interface exercisesOBJ {
 interface exercisesPromise extends exercisesOBJ {
   id: string;
 }
-
 export default function CreateExercise() {
   const [exerciseNumber, setExerciseNumber] = useState([1]);
-
   var workoutRef: string;
   useEffect(() => {
     async function getLatestWorkoutRef() {
@@ -36,14 +33,11 @@ export default function CreateExercise() {
     }
     getLatestWorkoutRef();
   }, []);
-
   const handleAddExerciseClick = () => {
     const newArray = [...exerciseNumber, 1];
     console.log(newArray);
-
     setExerciseNumber(newArray);
   };
-
   const handleRemoveExerciseClick = () => {
     if (exerciseNumber.length > 1) {
       const newArray = [...exerciseNumber];
@@ -51,15 +45,16 @@ export default function CreateExercise() {
       setExerciseNumber(newArray);
     }
   };
-
   // const handleSubmit = (e: React.SyntheticEvent) => {
   //   e.preventDefault();
   //   const event = e.target as HTMLFormElement;
   //   // const newExercises = [{}];
   // };
-
   return (
-    <Container maxWidth="md">
+    <Container maxWidth="md" sx={{ border: '5px solid #6296EA', borderRadius: '1.3rem' }}>
+      <Typography variant="h3" align="center" paragraph sx={{ fontSize: '2rem' }}>
+        Exercise Planner
+      </Typography>
       <form
         className={css.formStyle}
         onSubmit={(e: React.SyntheticEvent) => {
@@ -72,7 +67,6 @@ export default function CreateExercise() {
             const targetSets = event[index + 2] as HTMLInputElement;
             const targetReps = event[index + 4] as HTMLInputElement;
             const targetRest = event[index + 6] as HTMLInputElement;
-
             const exerciseObject: exercisesOBJ = {
               exercise_name: targetName.value,
               sets: Number(targetSets.value),
@@ -84,7 +78,6 @@ export default function CreateExercise() {
             index += 8;
             console.log(exercisesArray);
           });
-
           fetch('https://fit-trax-backend-main.vercel.app/api/exercises', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -146,32 +139,32 @@ export default function CreateExercise() {
           ))}
         </Grid>
         <Grid container>
-          <Grid item xs={6} sx={{ display: 'flex', justifyContent: 'center' }} className={css.gridMargin}>
+          <Grid
+            item
+            xs={6}
+            sx={{ display: 'flex', justifyContent: 'center', margin: '1rem 0' }}
+            className={css.gridMargin}
+          >
             <Button variant="outlined" sx={{ borderRadius: '1rem' }} onClick={handleRemoveExerciseClick}>
               <ClearIcon />
             </Button>
           </Grid>
-          <Grid item xs={6} sx={{ display: 'flex', justifyContent: 'center' }} className={css.gridMargin}>
+          <Grid
+            item
+            xs={6}
+            sx={{ display: 'flex', justifyContent: 'center', margin: '1rem 0' }}
+            className={css.gridMargin}
+          >
             <Button variant="contained" sx={{ borderRadius: '1rem' }} onClick={handleAddExerciseClick}>
               <AddIcon />
             </Button>
           </Grid>
-          <Grid
-            item
-            xs={6}
-            sx={{ display: 'flex', justifyContent: 'center', margin: '1rem 0' }}
-            className={css.gridMargin}
-          >
+          <Grid item xs={6} sx={{ display: 'flex', justifyContent: 'center' }} className={css.gridMargin}>
             <Button variant="outlined" sx={{ borderRadius: '1rem' }} color="secondary">
               <ArrowBackIosNewIcon />
             </Button>
           </Grid>
-          <Grid
-            item
-            xs={6}
-            sx={{ display: 'flex', justifyContent: 'center', margin: '1rem 0' }}
-            className={css.gridMargin}
-          >
+          <Grid item xs={6} sx={{ display: 'flex', justifyContent: 'center' }} className={css.gridMargin}>
             <Button type="submit" variant="contained" sx={{ borderRadius: '1rem' }} color="secondary">
               <DoneIcon />
             </Button>
