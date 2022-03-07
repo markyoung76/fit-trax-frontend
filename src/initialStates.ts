f8e4ec770ac4cbadb30b87d93b92f89eb655c9ac
E-K8 <ekatplat@gmail.com>
2022-03-07 14:21:33 +0000

    attempt to reveal the menu

--- src/initialStates.ts
+++ src/initialStates.ts
@@ -4,7 +4,7 @@ export const exerciseInitialState = [
     exercise_name: 'tri-angle pushups',
     sets: 3,
     reps: 20,
-    rest_period: '90 seconds',
+    rest_period: '90 sec',
     workout_ref: 'dbd0d2c1-84f9-4413-b4ed-eed2b266d10d',
   },
   {
@@ -12,7 +12,7 @@ export const exerciseInitialState = [
     exercise_name: 'Shoulder press (dumbbells)',
     sets: 3,
     reps: 10,
-    rest_period: '90 seconds',
+    rest_period: '90 sec',
     workout_ref: 'dbd0d2c1-84f9-4413-b4ed-eed2b266d10d',
   },
   {
@@ -20,7 +20,7 @@ export const exerciseInitialState = [
     exercise_name: 'pushups',
     sets: 2,
     reps: 20,
-    rest_period: '60 seconds',
+    rest_period: '60 sec',
     workout_ref: 'dbd0d2c1-84f9-4413-b4ed-eed2b266d10d',
   },
 ];
@@ -33,14 +33,14 @@ export const workoutInitialState = [
   },
   {
     id: '84b4d01d-7a8b-4be4-9345-8a77febaf70c',
-    workout_name: 'legs workout',
+    workout_name: 'Legs workout',
     workout_type: 'Lower Body',
-    workout_difficulty: 'beginner',
+    workout_difficulty: 'Beginner',
   },
   {
     id: '30406bf3-2c20-4413-88af-18967f21173d',
     workout_name: 'Pull workout',
     workout_type: 'Upper Body',
-    workout_difficulty: 'intermediate',
+    workout_difficulty: 'Intermediate',
   },
 ];
