7e1a5a2bc933936ed2d305870eb3170376a9cd2f
Jul-Me <memai.julian@gmail.com>
2022-03-08 20:51:16 +0000

    refactor(exercise-planner): refactored exercise planner component, used state and loading button

--- src/components/ExercisePlanner/ExercisePlannerStepOne/index.tsx
+++ src/components/ExercisePlanner/ExercisePlannerStepOne/index.tsx
@@ -1,97 +1,96 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import MenuItem from '@mui/material/MenuItem';
-import { WorkoutType, WorkoutDifficulty } from '../../../types';
-import { Button, Container } from '@mui/material';
-import { textFieldStyle, buttonStyle } from './styles';
 import { v4 as uuid } from 'uuid';
+import { Box, Container, TextField, MenuItem } from '@mui/material';
+import LoadingButton from '@mui/lab/LoadingButton';
+import { WorkoutType, WorkoutDifficulty } from '../../../types';
+import { containerStyle, textFieldStyle, buttonStyle, boxButtonStyle, inputsBoxStyle } from './styles';
 
 const workoutTypes = [WorkoutType.FullBody, WorkoutType.UpperBody, WorkoutType.LowerBody];
 const workoutDifficulties = [WorkoutDifficulty.Beginner, WorkoutDifficulty.Intermediate, WorkoutDifficulty.Advanced];
 
 const ExercisePlannerStepOne = (): JSX.Element => {
+  const [workoutType, setWorkoutType] = React.useState('');
+  const [workoutLevel, setWorkoutLevel] = React.useState('');
+  const [workoutName, setWorkoutName] = React.useState('');
+  const [isLoading, setIsLoading] = React.useState(false);
+
   const handleSubmit = (e: React.SyntheticEvent) => {
     e.preventDefault();
-    const workoutForm = e.target as HTMLFormElement;
-    const workoutName = workoutForm[0] as HTMLInputElement;
-    const workoutType = workoutForm[2] as HTMLInputElement;
-    const workoutLevel = workoutForm[4] as HTMLInputElement;
+    setIsLoading(true);
+
     const data = {
       id: uuid(),
-      workout_name: workoutName.value,
-      workout_type: workoutType.value,
-      workout_difficulty: workoutLevel.value,
+      workout_name: workoutName,
+      workout_type: workoutType,
+      workout_difficulty: workoutLevel,
     };
+
     fetch('https://fit-trax-backend-main.vercel.app/api/workouts', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(data),
+    }).then(() => {
+      setIsLoading(false);
+      setWorkoutType('');
+      setWorkoutLevel('');
+      setWorkoutName('');
     });
-    workoutName.value = '';
-    workoutType.value = '';
-    workoutLevel.value = '';
   };
 
   return (
-    <>
-      <Container fixed sx={textFieldStyle}>
-        <form
-          onSubmit={(e: React.SyntheticEvent) => {
-            handleSubmit(e);
-          }}
-        >
-          <Box
-            sx={{
-              '& > :not(style)': { m: 1, width: '55ch' },
-            }}
+    <Container maxWidth="sm" sx={containerStyle}>
+      <form onSubmit={handleSubmit}>
+        <Box sx={inputsBoxStyle}>
+          <TextField
+            id="workout-name"
+            required
+            label="Workout Name"
+            value={workoutName}
+            onChange={(e) => setWorkoutName(e.target.value)}
+            variant="outlined"
+            sx={textFieldStyle}
+          />
+
+          <TextField
+            id="workout-type"
+            select
+            required
+            label="Workout Type"
+            value={workoutType}
+            onChange={(e) => setWorkoutType(e.target.value)}
+            sx={textFieldStyle}
           >
-            <TextField
-              id="outlined-basic"
-              required
-              label="Workout Name"
-              variant="outlined"
-              sx={{ maxWidth: '100%', margin: '0.5rem 0' }}
-            />
-            <TextField
-              id="outlined-select-workoutType"
-              select
-              required
-              label="Workout Type"
-              sx={{ maxWidth: '100%', margin: '0.5rem 0' }}
-            >
-              {workoutTypes.map((workout) => {
-                return (
-                  <MenuItem key={workout} value={workout}>
-                    {workout}
-                  </MenuItem>
-                );
-              })}
-            </TextField>
-            <TextField
-              id="outlined-select-workoutDifficulty"
-              select
-              required
-              label="Workout Difficulty"
-              sx={{ maxWidth: '100%', margin: '0.5rem 0' }}
-            >
-              {workoutDifficulties.map((workoutLevel) => {
-                return (
-                  <MenuItem key={workoutLevel} value={workoutLevel}>
-                    {workoutLevel}
-                  </MenuItem>
-                );
-              })}
-            </TextField>
-          </Box>
-          <Box sx={{ display: 'flex', justifyContent: 'center', paddingTop: '3rem' }}>
-            <Button type="submit" variant="contained" sx={buttonStyle}>
-              Save
-            </Button>
-          </Box>
-        </form>
-      </Container>
-    </>
+            {workoutTypes.map((workout) => (
+              <MenuItem key={workout} value={workout}>
+                {workout}
+              </MenuItem>
+            ))}
+          </TextField>
+
+          <TextField
+            id="workout-difficulty"
+            select
+            required
+            label="Workout Difficulty"
+            value={workoutLevel}
+            onChange={(e) => setWorkoutLevel(e.target.value)}
+            sx={textFieldStyle}
+          >
+            {workoutDifficulties.map((workoutLevel) => (
+              <MenuItem key={workoutLevel} value={workoutLevel}>
+                {workoutLevel}
+              </MenuItem>
+            ))}
+          </TextField>
+        </Box>
+
+        <Box sx={boxButtonStyle}>
+          <LoadingButton type="submit" sx={buttonStyle} loading={isLoading} variant="contained">
+            Save
+          </LoadingButton>
+        </Box>
+      </form>
+    </Container>
   );
 };
 
--- src/components/ExercisePlanner/ExercisePlannerStepOne/styles.ts
+++ src/components/ExercisePlanner/ExercisePlannerStepOne/styles.ts
@@ -1,8 +1,24 @@
-export const textFieldStyle = {
-  width: '25ch',
+export const containerStyle = {
   display: 'flex',
   flexWrap: 'wrap',
   justifyContent: 'center',
+};
+
+export const inputsBoxStyle = {
+  '& > :not(style)': {
+    m: 1,
+    width: '55ch',
+  },
+};
+export const textFieldStyle = {
+  maxWidth: '100%',
+  margin: '0.5rem 0',
+};
+
+export const boxButtonStyle = {
+  display: 'flex',
+  justifyContent: 'center',
+  paddingTop: '3rem',
 };
 
 export const buttonStyle = {
